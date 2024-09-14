90c551df5ed6ed6e25491ae03c6c51630093a41f
ArlequinnoGit <UtilizatorRachkiov@yandex.ru>
2024-09-14 18:21:54 +0400

    Add files via upload

---
+++ Stroki&IndeksatsiaStrok.py
@@ -0,0 +1,18 @@
+#1st
+example = "Бесперспективно"
+
+#2nd
+print(example[:1])
+
+#3rd
+print(example[-1:])
+
+#4th
+print(example[8:15])
+
+#5th
+print(example[::-1])
+
+#6th
+print(example[::2])
+
