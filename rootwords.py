c5c41f60fc73acebd9891947b5a645a9ae34d542
Arlequinno/Dmitry <UtilizatorRachkiov@yandex.ru>
2024-11-14 17:09:43 +0400

    Update rootwords.py

--- rootwords.py
+++ rootwords.py
@@ -8,13 +8,13 @@ def single_root_words(root_word, *other_words):
     # Перебираем каждое слово из переданной последовательности
     for word in other_words:
         # Приводим текущее слово к нижнему регистру для сравнения без учета регистра
-        if root_word in word.lower():
-            # Если корневое слово содержится в текущем слове, добавляем его в результат
+        if root_word in word.lower() or word.lower() in root_word:
+            # Если корневое слово содержится в текущем слове ИЛИ текущее слово содержится в корневом, добавляем его в результат
             same_words.append(word)
 
     # Возвращаем найденные однокоренные слова
     return same_words
 
 # Вызываем функцию и выводим результат
-result = single_root_words("able", "disablement", "ABLE", "abler", "disabled")
+result = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 print(result)
